8d6c64ce57fd87935085c4a0e0cd79ce4df25146
Nada Ahmed <60941223+nadaahmed2001@users.noreply.github.com>
2023-03-08 15:06:39 +0200

    Update Maximum Perimeter Triangle.cpp

--- Hackerrank/Maximum Perimeter Triangle.cpp
+++ Hackerrank/Maximum Perimeter Triangle.cpp
@@ -3,25 +3,34 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> maximumPerimeterTriangle(vector<int> sticks) {
+vector<int> maximumPerimeterTriangle(vector<int> sticks)
+{
     bool validFound = false;
     vector<int>v;//final answer
     long long max = 0, sum = 0;
     //get all possible combinations
-    for (int i = 0; i < sticks.size(); i++) {
-        for (int j = i + 1; j < sticks.size(); j++) {
-            for (int k = j + 1; k < sticks.size(); k++) {
+    for (int i = 0; i < sticks.size(); i++)
+    {
+        for (int j = i + 1; j < sticks.size(); j++)
+        {
+            for (int k = j + 1; k < sticks.size(); k++)
+            {
                 //check if triangle is valid
                 if (sticks[i] + sticks[j] > sticks[k]
-                    && sticks[i] + sticks[k] > sticks[j]
-                    && sticks[k] + sticks[j] > sticks[i]) {//Valid found
+                        && sticks[i] + sticks[k] > sticks[j]
+                        && sticks[k] + sticks[j] > sticks[i])  //Valid found
+                {
                     validFound = true;
                     sum = sticks[i] + sticks[j] + sticks[k];
-                    while (sum < 0) sum += 1e9;
-                    if (sum > max) {
+                    while (sum < 0)
+                        sum += 1e9;
+                    if (sum > max)
+                    {
                         v.clear();
                         max = sum;
-                        v.push_back(sticks[i]); v.push_back(sticks[j]); v.push_back(sticks[k]);
+                        v.push_back(sticks[i]);
+                        v.push_back(sticks[j]);
+                        v.push_back(sticks[k]);
 
                     }
 
@@ -30,8 +39,10 @@ vector<int> maximumPerimeterTriangle(vector<int> sticks) {
         }
     }
     sort(v.begin(), v.end());
-    if (validFound == true)return v;
-    else return{ -1 };
+    if (validFound == true)
+        return v;
+    else
+        return{ -1 };
 
 }
 int main()
@@ -40,14 +51,16 @@ int main()
     cin >> n;
     int* sticks = new int[n];
     vector<int>v;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> sticks[i];
         v.push_back(sticks[i]);
 
     }
 
     vector<int> v1 = maximumPerimeterTriangle(v);
-    for (int i = 0; i < v1.size(); i++) {
+    for (int i = 0; i < v1.size(); i++)
+    {
         cout << v1.at(i) << " ";
     }
 
