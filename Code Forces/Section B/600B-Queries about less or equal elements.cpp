afe68d444aa098245b2226c180e6f20d1db62196
Nada Ahmed <60941223+nadaahmed2001@users.noreply.github.com>
2024-02-26 11:34:06 +0200

    Add files via upload

--- Code Forces/Section B/600B-Queries about less or equal elements.cpp
+++ Code Forces/Section B/600B-Queries about less or equal elements.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <deque>
+#include <map>
+#include <set>
+#include <string>
+#include <iomanip>
+#define endl "\n"
+#define ll long long
 using namespace std;
 
-int main()
-{
-    int n, m, elem,ans;
-    cin >> n >> m;
-    vector<int> v1;
-    vector<int> v2;
-    for (int i = 0; i < n; i++) {
-        cin >> elem;
-        v1.push_back(elem);
-    }
-    sort(v1.begin(), v1.end());
 
-    for (int i = 0; i < m; i++) {
-        cin >> elem; 
-        ans = upper_bound(v1.begin(), v1.end(), elem) - v1.begin();
-        cout << ans<<" ";
+
+void solve() {
+    int n, m, ans = 0;cin >> n >> m;
+    vector<int> a(n);
+    vector<int> b(m);
+    for (int i = 0;i < n;i++)cin >> a[i];
+    for (int i = 0;i < m;i++)cin >> b[i];
+    sort(a.begin(), a.end());
+    for (int i = 0;i < m;i++) {
+        vector<int> ::iterator t = upper_bound(a.begin(), a.end(), b[i]);
+        cout << t - a.begin() << " ";
     }
+    cout << endl;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
     return 0;
 }
